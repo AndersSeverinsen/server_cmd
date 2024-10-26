fd674df01a29222c8278de9aa68e32f08f8e3c14
au701790 <au701790@uni.au.dk>
2024-10-26 14:56:51 +0200

    js måske virker

--- web/web.js
+++ web/web.js
@@ -1,6 +1,21 @@
 
 
 
+function createLockers() {
+    let grid = document.querySelector(".big-grid")
+    for(let i = 0; i < 10; i++) {
+        let indicator = document.createElement("div")
+        indicator.classList.add("indicator")
+        let newLocker = document.createElement("div")
+        newLocker.classList.add("grid-item")
+        newLocker.classList.add("occupied")
+        newLocker.setAttribute("id", i)
+        newLocker.innerText = i
+        newLocker.appendChild(indicator)
+        grid.appendChild(newLocker)
+    }
+}
+
 
 function updateLockermap(lockerJson) {
     lockers = lockerJson.lockers;
@@ -8,23 +23,21 @@ function updateLockermap(lockerJson) {
 
 
 
-async function getLockerStatus() {
-    const response = await fetch("/status/", {
-        method: "GET", 
-        headers: {Accept: "application/json"},
-    })
-    if (!response.ok) {
-        throw new Error(
-            `Connection failed with status code ${response.status}`
-        );
-    }
-    log(response.json)
-    return response.json;
+function httpGet() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "http://localhost:8080/statusLocker", false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
 
 
 
 
+document.addEventListener("DOMContentLoaded", function(event){
+    createLockers()
+    res = httpGet()
+    console.log(res)
+  });
 
 
 
